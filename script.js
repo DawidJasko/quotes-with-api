db76db3b000aa77331d7a4cfffdf3570785b0759
DawidJasko <68279366+DawidJasko@users.noreply.github.com>
2023-01-14 02:03:08 +0100

    api

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 // Get Quote From API
 async function getQuote() {
+  const proxyUrl =
+    'https://cors-anywhere.herokuapp.com/https://phagesdb.org/api/sequenced_phages?page_size=99999999';
   const apiUrl =
-    'http://api.forismatic.com/api/1.0/?method=getQuote&=en&format=json';
+    'http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en';
   try {
-    const response = await fetch(apiUrl);
+    const response = await fetch(proxyUrl + apiUrl);
     const data = await response.json();
     console.log(data);
   } catch (error) {
+    getQuote();
     console.log('whoops, no quote', error);
   }
 }
